ca46a87d0dd5e8e4f4e424da4b900b13ca8999b1
Narek Stepanyan <narek.stepanyan@ramax.ru>
2019-09-13 19:24:59 +0300

    Подключены Rx библиотеки

---
+++ FrogogoTestApp/UsersList/UsersListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UsersListViewController.swift
+//  FrogogoTestApp
+//
+//  Created by Narek Stepanyan on 13/09/2019.
+//  Copyright © 2019 Narek Stepanyan. All rights reserved.
+//
+
+import UIKit
+
+class UsersListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'FrogogoTestApp' do
+  pod 'RxSwift', '~> 5'
+  pod 'RxCocoa', '~> 5'
+  pod 'RxDataSources', '~> 4.0'
+  pod 'Moya', '~> 13.0'
+end
